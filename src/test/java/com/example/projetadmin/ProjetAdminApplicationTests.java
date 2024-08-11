b9db5407e0f64deb04b90ee99293a95081f2d7e7
Mounahbaieb <mounahbaieb2020@gmail.com>
2024-08-11 22:00:31 +0100

    first commit

---
+++ src/test/java/com/example/projetadmin/ProjetAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
